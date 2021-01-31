93fcd2e33970069e7e3a9d673bce523e9c55e045
Kelly88888 <64625188+Kelly88888@users.noreply.github.com>
2021-01-31 10:56:49 -0500

    Add files via upload

---
+++ dragdrop.js
@@ -0,0 +1,76 @@
+var dropFileForm = document.getElementById("dropFileForm");
+var fileLabelText = document.getElementById("fileLabelText");
+var uploadStatus = document.getElementById("uploadStatus");
+var fileInput = document.getElementById("fileInput");
+var droppedFiles;
+
+function overrideDefault(event) {
+  event.preventDefault();
+  event.stopPropagation();
+}
+
+function fileHover() {
+  dropFileForm.classList.add("fileHover");
+}
+
+function fileHoverEnd() {
+  dropFileForm.classList.remove("fileHover");
+}
+
+function addFiles(event) {
+  droppedFiles = event.target.files || event.dataTransfer.files;
+  showFiles(droppedFiles);
+}
+
+function showFiles(files) {
+  if (files.length > 1) {
+    fileLabelText.innerText = files.length + " files selected";
+  } else {
+    fileLabelText.innerText = files[0].name;
+  }
+}
+
+function uploadFiles(event) {
+  event.preventDefault();
+  changeStatus("Uploading...");
+
+  var formData = new FormData();
+
+  for (var i = 0, file; (file = droppedFiles[i]); i++) {
+    formData.append(fileInput.name, file, file.name);
+  }
+
+  var xhr = new XMLHttpRequest();
+  xhr.onreadystatechange = function(data) {
+    //handle server response and change status of
+    //upload process via changeStatus(text)
+    console.log(xhr.response);
+  };
+  xhr.open(dropFileForm.method, dropFileForm.action, true);
+  xhr.send(formData);
+}
+
+function changeStatus(text) {
+  uploadStatus.innerText = text;
+}
+
+
+//Get the button:
+mybutton = document.getElementById("myBtn");
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+  document.body.scrollTop = 0; // For Safari
+  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+}
